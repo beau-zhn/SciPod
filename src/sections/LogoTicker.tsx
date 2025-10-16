92b169d0796b723a003ca2d7c1a46eb61a1a2f46
beau-zhn <a.tolegen@nu.edu.kz>
2025-10-16 14:16:46 +0500

    Started Modifying logoticker

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,25 @@
+import spotifyLogo from '@/assets/logo-spotify.png'
+import youtubeLogo from'@/assets/logo-youtube.png'
+import googleLogo from '@/assets/logo-google.png'
+import appleLogo from '@/assets/logo-apple-podcasts.png'
+import amazonLogo from '@/assets/logo-amazon.png'
+
+
 export const LogoTicker = () => {
-  return <section>LogoTicker</section>;
+  return <section>
+    <div className="container">
+      <div>
+        <h2> Tuned in by the curious. Inspired by </h2>
+        <div>
+              {[spotifyLogo, 
+              youtubeLogo, 
+              googleLogo, 
+              appleLogo,amazonLogo,
+            ].map((logo) => (
+                 <img src={logo.src} key={logo.src} className='' />
+              ))}
+        </div>
+      </div>
+    </div>
+  </section>;
 };
